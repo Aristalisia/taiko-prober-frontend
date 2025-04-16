b1c58b4b4384a1b9eb69a1c7d95d04db9375f385
ArisReon <ArisReon@outlook.com>
2025-04-16 23:28:46 +0800

    新增了vitepress基础的简单docs框架

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: '叽奇NET',
+  description: '敲击太鼓开始游戏',
+  themeConfig: {
+    logo: '/images/logo.png',
+    nav: [
+      { text: '欢迎', link: '/' },
+      { text: '开始使用', link: '/getting-started' },
+      { text: '常见问题', link: '/faq' }
+    ],
+    search: {
+        provider: 'algolia',
+        options: {
+          appId: 'waiting',
+          apiKey: 'waiting',
+          indexName: 'deving'
+        }
+      },
+    sidebar: {
+      '/guide/': [
+        {
+          text: '简介',
+          items: [
+            { text: '安装与配置', link: '/guide/installation' },
+            { text: '使用示例', link: '/guide/usage' }
+          ]
+        }
+      ]
+    }
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,13 @@
+---
+layout: home
+
+hero:
+  name: 叽奇
+  tagline: 多功能，多平台支持的太鼓の達人工具机器人
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: ./guide/
+  image:
+      src: ./images/叽奇_BG.png
+
